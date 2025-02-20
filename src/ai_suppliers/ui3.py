27e930c7d3ec7633efc57724d1f7baf1425f51eb
LV-awais <leverify.bi035@gmail.com>
2025-02-20 15:42:49 +0500

    Updated crewai

--- src/ai_suppliers/ui3.py
+++ src/ai_suppliers/ui3.py
@@ -15,7 +15,7 @@ import time
 import threading
 import pandas as pd  # Required for better table handling
 
-from crew_class import  AiSuppliers
+from crewai import  AiSuppliers
 logo_url = os.path.join(BASE_DIR, "search.jpg")
 
 # Set Streamlit page configuration
